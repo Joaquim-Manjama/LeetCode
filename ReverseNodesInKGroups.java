eb8c1fd268c3e2ae42db3e8037c9c0b810d0c546
Joaquim Manjama <melorafaelmanjama@gmail.com>
2025-12-07 18:56:21 +0000

    week 10

---
+++ ReverseNodesInKGroups.java
@@ -0,0 +1,59 @@
+package LeetCode;
+
+public class ReverseNodesInKGroups {
+
+	// Given the head of a linked list, reverse the nodes of the list k at a time,
+	// and return the modified list.
+	public static ListNode reverseKGroup(ListNode head, int k) {
+		ListNode dummy = new ListNode(-1, head);
+		ListNode[] nodes = new ListNode[k];
+		ListNode first = dummy.next;
+		ListNode temp = dummy;
+		boolean broke = false;
+
+		while (first != null) {
+			broke = false;
+
+			for (int i = 0; i < k; i++) {
+				nodes[i] = first;
+				first = first.next;
+
+				if (first == null && i < k - 1) {
+					broke = true;
+					break;
+				}
+			}
+
+			if (first == null && broke) {
+				return dummy.next;
+			}
+
+			for (int i = k - 1; i >= 0; i--) {
+				temp.next = nodes[i];
+				temp = temp.next;
+			}
+			temp.next = first;
+		}
+		return dummy.next;
+	}
+
+	private static void display(ListNode list) {
+		StringBuilder output = new StringBuilder("[ ");
+		ListNode head = list;
+
+		while (head != null) {
+
+			output.append(head.val);
+
+			if (head.next != null) {
+				output.append(" -> ");
+			}
+
+			head = head.next;
+		}
+
+		output.append(" ]");
+
+		System.out.println(output);
+	}
+}
--- Solution.java
+++ Solution.java
@@ -3,6 +3,8 @@ package LeetCode;
 public class Solution {
 
 	public static void main(String[] args) {
-		ListNode list = new ListNode(1, new ListNode(2, new ListNode(3)));
+		ListNode list = new ListNode(1, new ListNode(2));//new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+		int k = 2;
+		
 	}
 }
